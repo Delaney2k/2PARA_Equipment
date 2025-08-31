df6751ebb91d248840d437fec72ecad411d5fdd0
Delaney2k <Pwddyp@gmail.com>
2025-08-31 03:39:38 +0100

    V1.0.5
    Fucked up earlier mb

--- Helmets/Config.cpp
+++ Helmets/Config.cpp
@@ -48,7 +48,8 @@ class CfgWeapons {
         
         };
         ace_hearing_protection = 1;
-        ace_hearing_lowerVolume = 0.25;
+        ace_hearing_hasEHP = 1;
+        ace_hearing_lowerVolume = 0.50; 
         class ItemInfo: HeadgearItem {
             mass = 31;
             uniformModel = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh.p3d";
@@ -438,6 +439,461 @@ class CfgWeapons {
         };
         hiddenSelectionsTextures[] = {
           "2PARA_Equipment\Helmets\Data\LCpl_CO.paa","2PARA_Equipment\Helmets\Data\16AA_DARK_CO.paa","2PARA_Equipment\Helmets\Data\SIGS_CO.paa"
+        
+        };
+    };
+    class RLC_Cobra_D: 2PARA_Cobra {
+        scope = 2;
+        weaponPoolAvailable = 1;
+        displayName = "[16AA RLC] Cobra(Mesh) Dark";
+        Picture = "\2PARA_Equipment\ui\2PARA_CA.paa";
+        model = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh.p3d";
+        hiddenSelections[] = {
+           "_RANK","_TRFHL","_TRFHR"
+          
+        };
+        hiddenSelectionsTextures[] = {
+          "","2PARA_Equipment\Helmets\Data\16AA_DARK_CO.paa","2PARA_Equipment\Helmets\Data\16AA_RLC_CO.paa"
+        
+        };
+    };
+    class 2PARA_Cobra_B: ItemCore {
+        author = "PJHQ";
+        scope = 2;
+        weaponPoolAvailable = 1;
+        displayName = "[2PARA] Cobra B(Mesh)";
+        Picture = "\2PARA_Equipment\ui\2PARA_CA.paa";
+        model = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh_B.p3d";
+        hiddenSelections[] = {
+           "_RANK","_TRFHL","_TRFHR"
+          
+        };
+        hiddenSelectionsTextures[] = {
+            "","2PARA_Equipment\Helmets\Data\16AA_CO.paa","2PARA_Equipment\Helmets\Data\2PARA_CO.paa"
+        
+        };
+        ace_hearing_protection = 1;
+        ace_hearing_hasEHP = 1;
+        ace_hearing_lowerVolume = 0.50;    
+        class ItemInfo: HeadgearItem {
+            mass = 31;
+            uniformModel = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh_B.p3d";
+            hiddenSelections[] = {
+               "_RANK","_TRFHL","_TRFHR"
+               
+            };
+            modelSides[] = {
+                3,
+                2,
+                1,
+                0
+            };
+            class HitpointsProtectionInfo {
+                class Head {
+                    hitPointName = "HitHead";
+                      armor = 8;
+                    passThrough = 0.5;
+                };
+            };
+        };
+    };
+    class 2PARA_Cobra_LCpl_B: 2PARA_Cobra_B {
+        scope = 2;
+        weaponPoolAvailable = 1;
+        displayName = "[2PARA] Cobra B(Mesh) LCpl";
+        Picture = "\2PARA_Equipment\ui\2PARA_CA.paa";
+        model = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh_B.p3d";
+        hiddenSelections[] = {
+           "_RANK","_TRFHL","_TRFHR"
+          
+        };
+        hiddenSelectionsTextures[] = {
+            "2PARA_Equipment\Helmets\Data\LCpl_CO.paa","2PARA_Equipment\Helmets\Data\16AA_CO.paa","2PARA_Equipment\Helmets\Data\2PARA_CO.paa"
+        
+        };
+    };
+    class 2PARA_Cobra_Cpl_B: 2PARA_Cobra_B {
+        scope = 2;
+        weaponPoolAvailable = 1;
+        displayName = "[2PARA] Cobra B(Mesh) Cpl";
+        Picture = "\2PARA_Equipment\ui\2PARA_CA.paa";
+        model = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh_B.p3d";
+        hiddenSelections[] = {
+           "_RANK","_TRFHL","_TRFHR"
+          
+        };
+        hiddenSelectionsTextures[] = {
+            "2PARA_Equipment\Helmets\Data\Cpl_CO.paa","2PARA_Equipment\Helmets\Data\16AA_CO.paa","2PARA_Equipment\Helmets\Data\2PARA_CO.paa"
+        
+        };
+    };
+    class 2PARA_Cobra_Sgt_B: 2PARA_Cobra_B {
+        scope = 2;
+        weaponPoolAvailable = 1;
+        displayName = "[2PARA] Cobra B(Mesh) Sgt";
+        Picture = "\2PARA_Equipment\ui\2PARA_CA.paa";
+        model = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh_B.p3d";
+        hiddenSelections[] = {
+           "_RANK","_TRFHL","_TRFHR"
+          
+        };
+        hiddenSelectionsTextures[] = {
+            "2PARA_Equipment\Helmets\Data\Sgt_CO.paa","2PARA_Equipment\Helmets\Data\16AA_CO.paa","2PARA_Equipment\Helmets\Data\2PARA_CO.paa"
+        };
+    };
+     class 2PARA_Cobra_SSgt_B: 2PARA_Cobra_B {
+        scope = 2;
+        weaponPoolAvailable = 1;
+        displayName = "[2PARA] Cobra B(Mesh) SSgt";
+        Picture = "\2PARA_Equipment\ui\2PARA_CA.paa";
+        model = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh_B.p3d";
+        hiddenSelections[] = {
+           "_RANK","_TRFHL","_TRFHR"
+          
+        };
+        hiddenSelectionsTextures[] = {
+            "2PARA_Equipment\Helmets\Data\SSgt_CO.paa","2PARA_Equipment\Helmets\Data\16AA_CO.paa","2PARA_Equipment\Helmets\Data\2PARA_CO.paa"
+        
+        };
+    };
+    class 2PARA_Cobra_2Lt_B: 2PARA_Cobra_B {
+        scope = 2;
+        weaponPoolAvailable = 1;
+        displayName = "[2PARA] Cobra B(Mesh) 2Lt";
+        Picture = "\2PARA_Equipment\ui\2PARA_CA.paa";
+        model = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh_B.p3d";
+        hiddenSelections[] = {
+           "_RANK","_TRFHL","_TRFHR"
+          
+        };
+        hiddenSelectionsTextures[] = {
+            "2PARA_Equipment\Helmets\Data\2Lt_CO.paa","2PARA_Equipment\Helmets\Data\16AA_CO.paa","2PARA_Equipment\Helmets\Data\2PARA_CO.paa"
+        
+        };
+    };
+    class 2PARA_Cobra_Lt_B: 2PARA_Cobra_B {
+        scope = 2;
+        weaponPoolAvailable = 1;
+        displayName = "[2PARA] Cobra B(Mesh) Lt";
+        Picture = "\2PARA_Equipment\ui\2PARA_CA.paa";
+        model = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh_B.p3d";
+        hiddenSelections[] = {
+           "_RANK","_TRFHL","_TRFHR"
+          
+        };
+        hiddenSelectionsTextures[] = {
+            "2PARA_Equipment\Helmets\Data\Lt_CO.paa","2PARA_Equipment\Helmets\Data\16AA_CO.paa","2PARA_Equipment\Helmets\Data\2PARA_CO.paa"
+        
+        };
+    };
+    class 2PARA_Cobra_B_D: 2PARA_Cobra_B {
+        scope = 2;
+        weaponPoolAvailable = 1;
+        displayName = "[2PARA] Cobra B(Mesh) Dark";
+        Picture = "\2PARA_Equipment\ui\2PARA_CA.paa";
+        model = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh_B.p3d";
+        hiddenSelections[] = {
+           "_RANK","_TRFHL","_TRFHR"
+          
+        };
+        hiddenSelectionsTextures[] = {
+            "","2PARA_Equipment\Helmets\Data\16AA_DARK_CO.paa","2PARA_Equipment\Helmets\Data\2PARA_CO.paa"
+        
+        };
+    };
+    class 2PARA_Cobra_LCpl_B_D: 2PARA_Cobra_B {
+        scope = 2;
+        weaponPoolAvailable = 1;
+        displayName = "[2PARA] Cobra B(Mesh) LCpl Dark";
+        Picture = "\2PARA_Equipment\ui\2PARA_CA.paa";
+        model = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh_B.p3d";
+        hiddenSelections[] = {
+           "_RANK","_TRFHL","_TRFHR"
+          
+        };
+        hiddenSelectionsTextures[] = {
+            "2PARA_Equipment\Helmets\Data\LCpl_CO.paa","2PARA_Equipment\Helmets\Data\16AA_DARK_CO.paa","2PARA_Equipment\Helmets\Data\2PARA_CO.paa"
+        
+        };
+    };
+    class 2PARA_Cobra_Cpl_B_D: 2PARA_Cobra_B {
+        scope = 2;
+        weaponPoolAvailable = 1;
+        displayName = "[2PARA] Cobra B(Mesh) Cpl Dark";
+        Picture = "\2PARA_Equipment\ui\2PARA_CA.paa";
+        model = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh_B.p3d";
+        hiddenSelections[] = {
+           "_RANK","_TRFHL","_TRFHR"
+          
+        };
+        hiddenSelectionsTextures[] = {
+            "2PARA_Equipment\Helmets\Data\Cpl_CO.paa","2PARA_Equipment\Helmets\Data\16AA_DARK_CO.paa","2PARA_Equipment\Helmets\Data\2PARA_CO.paa"
+        
+        };
+    };
+    class 2PARA_Cobra_Sgt_B_D: 2PARA_Cobra_B {
+        scope = 2;
+        weaponPoolAvailable = 1; 
+        displayName = "[2PARA] Cobra B(Mesh) Sgt Dark";
+        Picture = "\2PARA_Equipment\ui\2PARA_CA.paa";
+        model = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh_B.p3d";
+        hiddenSelections[] = {
+           "_RANK","_TRFHL","_TRFHR"
+          
+        };
+        hiddenSelectionsTextures[] = {
+            "2PARA_Equipment\Helmets\Data\Sgt_CO.paa","2PARA_Equipment\Helmets\Data\16AA_DARK_CO.paa","2PARA_Equipment\Helmets\Data\2PARA_CO.paa"
+        };
+    };
+     class 2PARA_Cobra_SSgt_B_D: 2PARA_Cobra_B {
+        scope = 2;
+        weaponPoolAvailable = 1;
+        displayName = "[2PARA] Cobra B(Mesh) SSgt Dark";
+        Picture = "\2PARA_Equipment\ui\2PARA_CA.paa";
+        model = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh_B.p3d";
+        hiddenSelections[] = {
+           "_RANK","_TRFHL","_TRFHR"
+          
+        };
+        hiddenSelectionsTextures[] = {
+            "2PARA_Equipment\Helmets\Data\SSgt_CO.paa","2PARA_Equipment\Helmets\Data\16AA_DARK_CO.paa","2PARA_Equipment\Helmets\Data\2PARA_CO.paa"
+        
+        };
+    };
+    class 2PARA_Cobra_2Lt_B_D: 2PARA_Cobra_B {
+        scope = 2;
+        weaponPoolAvailable = 1;
+        displayName = "[2PARA] Cobra B(Mesh) 2Lt Dark";
+        Picture = "\2PARA_Equipment\ui\2PARA_CA.paa";
+        model = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh_B.p3d";
+        hiddenSelections[] = {
+           "_RANK","_TRFHL","_TRFHR"
+          
+        };
+        hiddenSelectionsTextures[] = {
+            "2PARA_Equipment\Helmets\Data\2Lt_CO.paa","2PARA_Equipment\Helmets\Data\16AA_Dark_CO.paa","2PARA_Equipment\Helmets\Data\2PARA_CO.paa"
+        
+        };
+    };
+    class 2PARA_Cobra_Lt_B_D: 2PARA_Cobra_B {
+        scope = 2;
+        weaponPoolAvailable = 1;
+        displayName = "[2PARA] Cobra B(Mesh) Lt Dark";
+        Picture = "\2PARA_Equipment\ui\2PARA_CA.paa";
+        model = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh_B.p3d";
+        hiddenSelections[] = {
+           "_RANK","_TRFHL","_TRFHR"
+          
+        };
+        hiddenSelectionsTextures[] = {
+            "2PARA_Equipment\Helmets\Data\Lt_CO.paa","2PARA_Equipment\Helmets\Data\16AA_Dark_CO.paa","2PARA_Equipment\Helmets\Data\2PARA_CO.paa"
+        
+        };
+    };
+    class RAMC_Cobra_B: 2PARA_Cobra_B {
+        scope = 2;
+        weaponPoolAvailable = 1;
+        displayName = "[2PARA/RAMC] Cobra B(Mesh)";
+        Picture = "\2PARA_Equipment\ui\2PARA_CA.paa";
+        model = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh_B.p3d";
+        hiddenSelections[] = {
+           "_RANK","_TRFHL","_TRFHR"
+          
+        };
+        hiddenSelectionsTextures[] = {
+           "","2PARA_Equipment\Helmets\Data\16AA_CO.paa","2PARA_Equipment\Helmets\Data\RAMC_CO.paa"
+        
+        };
+    };class RAMC_Cobra_D_B: 2PARA_Cobra_B {
+        scope = 2;
+        weaponPoolAvailable = 1;
+        displayName = "[2PARA/RAMC] Cobra B(Mesh) Dark";
+        Picture = "\2PARA_Equipment\ui\2PARA_CA.paa";
+        model = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh_B.p3d";
+        hiddenSelections[] = {
+           "_RANK","_TRFHL","_TRFHR"
+          
+        };
+        hiddenSelectionsTextures[] = {
+           "","2PARA_Equipment\Helmets\Data\16AA_DARK_CO.paa","2PARA_Equipment\Helmets\Data\RAMC_CO.paa"
+        
+        };
+    };
+    class RAMC_Cobra_LCpl_B: 2PARA_Cobra_B {
+        scope = 2;
+        weaponPoolAvailable = 1;
+        displayName = "[2PARA/RAMC] Cobra B(Mesh) LCpl";
+        Picture = "\2PARA_Equipment\ui\2PARA_CA.paa";
+        model = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh_B.p3d";
+        hiddenSelections[] = {
+           "_RANK","_TRFHL","_TRFHR"
+          
+        };
+        hiddenSelectionsTextures[] = {
+          "2PARA_Equipment\Helmets\Data\LCpl_CO.paa","2PARA_Equipment\Helmets\Data\16AA_CO.paa","2PARA_Equipment\Helmets\Data\RAMC_CO.paa"
+        
+        };
+    };
+     class RAMC_Cobra_LCpl_D_B: 2PARA_Cobra_B {
+        scope = 2;
+        weaponPoolAvailable = 1; 
+        displayName = "[2PARA/RAMC] Cobra B(Mesh) LCpl Dark";
+        Picture = "\2PARA_Equipment\ui\2PARA_CA.paa";
+        model = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh_B.p3d";
+        hiddenSelections[] = {
+           "_RANK","_TRFHL","_TRFHR"
+          
+        };
+        hiddenSelectionsTextures[] = {
+          "2PARA_Equipment\Helmets\Data\LCpl_CO.paa","2PARA_Equipment\Helmets\Data\16AA_DARK_CO.paa","2PARA_Equipment\Helmets\Data\RAMC_CO.paa"
+        
+        };
+    };
+    class RAMC_Cobra_Cpl_B: 2PARA_Cobra_B {
+        scope = 2;
+        weaponPoolAvailable = 1;
+        displayName = "[2PARA/RAMC] Cobra B(Mesh) Cpl";
+        Picture = "\2PARA_Equipment\ui\2PARA_CA.paa";
+        model = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh_B.p3d";
+        hiddenSelections[] = {
+           "_RANK","_TRFHL","_TRFHR"
+          
+        };
+        hiddenSelectionsTextures[] = {
+           "2PARA_Equipment\Helmets\Data\Cpl_CO.paa","2PARA_Equipment\Helmets\Data\16AA_CO.paa","2PARA_Equipment\Helmets\Data\RAMC_CO.paa"
+        
+        };
+    };
+    class RAMC_Cobra_Cpl_B_D: 2PARA_Cobra_B {
+        scope = 2;
+        weaponPoolAvailable = 1;
+        displayName = "[2PARA/RAMC] Cobra B(Mesh) Cpl Dark";
+        Picture = "\2PARA_Equipment\ui\2PARA_CA.paa";
+        model = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh_B.p3d";
+        hiddenSelections[] = {
+           "_RANK","_TRFHL","_TRFHR"
+          
+        };
+        hiddenSelectionsTextures[] = {
+           "2PARA_Equipment\Helmets\Data\Cpl_CO.paa","2PARA_Equipment\Helmets\Data\16AA_DARK_CO.paa","2PARA_Equipment\Helmets\Data\RAMC_CO.paa"
+        
+        };
+    };
+        class RAMC_Cobra_Sgt_B: 2PARA_Cobra_B {
+        scope = 2;
+        weaponPoolAvailable = 1;
+        displayName = "[2PARA/RAMC] Cobra B(Mesh) Sgt";
+        Picture = "\2PARA_Equipment\ui\2PARA_CA.paa";
+        model = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh_B.p3d";
+        hiddenSelections[] = {
+           "_RANK","_TRFHL","_TRFHR"
+          
+        };
+        hiddenSelectionsTextures[] = {
+            "2PARA_Equipment\Helmets\Data\Sgt_CO.paa","2PARA_Equipment\Helmets\Data\16AA_CO.paa","2PARA_Equipment\Helmets\Data\RAMC_CO.paa"
+        };
+    };
+        class RAMC_Cobra_Sgt_B_D: 2PARA_Cobra_B {
+        scope = 2;
+        weaponPoolAvailable = 1;
+        displayName = "[2PARA/RAMC] Cobra B(Mesh) Sgt Dark";
+        Picture = "\2PARA_Equipment\ui\2PARA_CA.paa";
+        model = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh_B.p3d";
+        hiddenSelections[] = {
+           "_RANK","_TRFHL","_TRFHR"
+          
+        };
+        hiddenSelectionsTextures[] = {
+            "2PARA_Equipment\Helmets\Data\Sgt_CO.paa","2PARA_Equipment\Helmets\Data\16AA_DARK_CO.paa","2PARA_Equipment\Helmets\Data\RAMC_CO.paa"
+        };
+    };
+     class SIG_Cobra_B: 2PARA_Cobra_B {
+        scope = 2;
+        weaponPoolAvailable = 1;
+        displayName = "[2PARA/SIGS] Cobra B(Mesh)";
+        Picture = "\2PARA_Equipment\ui\2PARA_CA.paa";
+        model = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh_B.p3d";
+        hiddenSelections[] = {
+           "_RANK","_TRFHL","_TRFHR"
+          
+        };
+        hiddenSelectionsTextures[] = {
+           "","2PARA_Equipment\Helmets\Data\16AA_CO.paa","2PARA_Equipment\Helmets\Data\SIGS_CO.paa"
+        
+        };
+    };
+    class SIG_Cobra_B_D: 2PARA_Cobra_B {
+        scope = 2;
+        weaponPoolAvailable = 1;
+        displayName = "[2PARA/SIGS] Cobra B(Mesh) Dark";
+        Picture = "\2PARA_Equipment\ui\2PARA_CA.paa";
+        model = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh_B.p3d";
+        hiddenSelections[] = {
+           "_RANK","_TRFHL","_TRFHR"
+          
+        };
+        hiddenSelectionsTextures[] = {
+           "","2PARA_Equipment\Helmets\Data\16AA_DARK_CO.paa","2PARA_Equipment\Helmets\Data\SIGS_CO.paa"
+        
+        };
+    };
+    class SIGS_Cobra_B_LCpl: 2PARA_Cobra_B {
+        scope = 2;
+        weaponPoolAvailable = 1;
+        displayName = "[2PARA/SIGS] Cobra B(Mesh) LCpl";
+        Picture = "\2PARA_Equipment\ui\2PARA_CA.paa";
+        model = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh_B.p3d";
+        hiddenSelections[] = {
+           "_RANK","_TRFHL","_TRFHR"
+          
+        };
+        hiddenSelectionsTextures[] = {
+          "2PARA_Equipment\Helmets\Data\LCpl_CO.paa","2PARA_Equipment\Helmets\Data\16AA_CO.paa","2PARA_Equipment\Helmets\Data\SIGS_CO.paa"
+        
+        };
+    };
+    class SIGS_Cobra_LCpl_B_D: 2PARA_Cobra_B {
+        scope = 2;
+        weaponPoolAvailable = 1;
+        displayName = "[2PARA/SIGS] Cobra B(Mesh) LCpl Dark";
+        Picture = "\2PARA_Equipment\ui\2PARA_CA.paa";
+        model = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh_B.p3d";
+        hiddenSelections[] = {
+           "_RANK","_TRFHL","_TRFHR"
+          
+        };
+        hiddenSelectionsTextures[] = {
+          "2PARA_Equipment\Helmets\Data\LCpl_CO.paa","2PARA_Equipment\Helmets\Data\16AA_DARK_CO.paa","2PARA_Equipment\Helmets\Data\SIGS_CO.paa"
+        
+        };
+    };
+    class RLC_Cobra_B_D: 2PARA_Cobra_B {
+        scope = 2;
+        weaponPoolAvailable = 1;
+        displayName = "[16AA RLC] Cobra B(Mesh) Dark";
+        Picture = "\2PARA_Equipment\ui\2PARA_CA.paa";
+        model = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_Mesh_B.p3d";
+        hiddenSelections[] = {
+           "_RANK","_TRFHL","_TRFHR"
+          
+        };
+        hiddenSelectionsTextures[] = {
+          "","2PARA_Equipment\Helmets\Data\16AA_DARK_CO.paa","2PARA_Equipment\Helmets\Data\16AA_RLC_CO.paa"
+        
+        };
+    };
+    class COBRA_ED: 2PARA_Cobra_B {
+        scope = 2;
+        weaponPoolAvailable = 1;
+        displayName = "[16AA] Cobra (ED TEST) Dark";
+        Picture = "\2PARA_Equipment\ui\2PARA_CA.paa";
+        model = "2PARA_Equipment\Helmets\Models\2PARA_Cobra_ED.p3d";
+        hiddenSelections[] = {
+           "_RANK","_TRFHL","_TRFHR"
+          
+        };
+        hiddenSelectionsTextures[] = {
+          "","2PARA_Equipment\Helmets\Data\16AA_DARK_CO.paa","2PARA_Equipment\Helmets\Data\16AA_RLC_CO.paa"
         
         };
     };
--- Uniforms/Config.cpp
+++ Uniforms/Config.cpp
@@ -18,11 +18,11 @@ class CfgVehicles {
     class I_Soldier_base_F;
     class SoldierWB;
     class Civilian_F;
-    class SCOTT_ARMY_G4_soldier: SoldierWB {
+    class SCOTT_ARMY_G4RS_soldier:SoldierWB {
         scope = 1;
         picture = "\2PARA_Equipment\Ui\2PARA_CA.paa";
-        model = "\2PARA_Equipment\Uniforms\Models\G4.p3d";
-        uniformClass = "SCOTT_ARMY_G4_soldier";
+        model = "\2PARA_Equipment\Uniforms\Models\G4RS.p3d";
+        uniformClass = "SCOTT_ARMY_G4RS_soldier";
         class Wounds {
             tex[] = {};
             mat[] = {
@@ -50,11 +50,75 @@ class CfgVehicles {
             };
         };
     };
-    class SCOTT_ARMY_G4RS_soldier: SoldierWB {
+     class 16AA_RLC_G4RS_soldier:SoldierWB {
+        scope = 1;
+        picture = "\2PARA_Equipment\Ui\2PARA_CA.paa";
+        model = "\2PARA_Equipment\Uniforms\Models\G4RS_16AA_RLC.p3d";
+        uniformClass = "16AA_RLC_G4RS_soldier";
+        class Wounds {
+            tex[] = {};
+            mat[] = {
+                "2PARA_Equipment\Uniforms\Data\_G4RS.rvmat",
+                "2PARA_Equipment\Uniforms\Data\_G4RS.rvmat",
+                "2PARA_Equipment\Uniforms\Data\_G4RS.rvmat",
+                "2PARA_Equipment\Uniforms\Data\_G4P.rvmat",
+                "2PARA_Equipment\Uniforms\Data\_G4P.rvmat",
+                "2PARA_Equipment\Uniforms\Data\_G4P.rvmat",
+                "A3\Characters_F\Heads\Data\hl_white_bald_muscular.rvmat",
+                "A3\Characters_F\Heads\Data\hl_white_bald_muscular_injury.rvmat",
+                "A3\Characters_F\Heads\Data\hl_white_bald_muscular_injury.rvmat",
+                "A3\Characters_F\Heads\Data\hl_black_bald_muscular.rvmat",
+                "A3\Characters_F\Heads\Data\hl_black_bald_muscular_injury.rvmat",
+                "A3\Characters_F\Heads\Data\hl_black_bald_muscular_injury.rvmat",
+                "A3\Characters_F\Heads\Data\hl_white_hairy_muscular.rvmat",
+                "A3\Characters_F\Heads\Data\hl_white_hairy_muscular_injury.rvmat",
+                "A3\Characters_F\Heads\Data\hl_white_hairy_muscular_injury.rvmat",
+                "A3\Characters_F\Heads\Data\hl_white_old.rvmat",
+                "A3\Characters_F\Heads\Data\hl_white_old_injury.rvmat",
+                "A3\Characters_F\Heads\Data\hl_white_old_injury.rvmat",
+                "A3\Characters_F\Heads\Data\hl_asian_bald_muscular.rvmat",
+                "A3\Characters_F\Heads\Data\hl_asian_bald_muscular_injury.rvmat",
+                "A3\Characters_F\Heads\Data\hl_asian_bald_muscular_injury.rvmat"
+            };
+        };
+    };
+     class 16AA_RAMC_G4RS_soldier:SoldierWB {
         scope = 1;
         picture = "\2PARA_Equipment\Ui\2PARA_CA.paa";
         model = "\2PARA_Equipment\Uniforms\Models\G4RS.p3d";
-        uniformClass = "SCOTT_ARMY_G4RS_soldier";
+        uniformClass = "16AA_RAMC_G4RS_soldier";
+        class Wounds {
+            tex[] = {};
+            mat[] = {
+                "2PARA_Equipment\Uniforms\Data\_G4RS.rvmat",
+                "2PARA_Equipment\Uniforms\Data\_G4RS.rvmat",
+                "2PARA_Equipment\Uniforms\Data\_G4RS.rvmat",
+                "2PARA_Equipment\Uniforms\Data\_G4P.rvmat",
+                "2PARA_Equipment\Uniforms\Data\_G4P.rvmat",
+                "2PARA_Equipment\Uniforms\Data\_G4P.rvmat",
+                "A3\Characters_F\Heads\Data\hl_white_bald_muscular.rvmat",
+                "A3\Characters_F\Heads\Data\hl_white_bald_muscular_injury.rvmat",
+                "A3\Characters_F\Heads\Data\hl_white_bald_muscular_injury.rvmat",
+                "A3\Characters_F\Heads\Data\hl_black_bald_muscular.rvmat",
+                "A3\Characters_F\Heads\Data\hl_black_bald_muscular_injury.rvmat",
+                "A3\Characters_F\Heads\Data\hl_black_bald_muscular_injury.rvmat",
+                "A3\Characters_F\Heads\Data\hl_white_hairy_muscular.rvmat",
+                "A3\Characters_F\Heads\Data\hl_white_hairy_muscular_injury.rvmat",
+                "A3\Characters_F\Heads\Data\hl_white_hairy_muscular_injury.rvmat",
+                "A3\Characters_F\Heads\Data\hl_white_old.rvmat",
+                "A3\Characters_F\Heads\Data\hl_white_old_injury.rvmat",
+                "A3\Characters_F\Heads\Data\hl_white_old_injury.rvmat",
+                "A3\Characters_F\Heads\Data\hl_asian_bald_muscular.rvmat",
+                "A3\Characters_F\Heads\Data\hl_asian_bald_muscular_injury.rvmat",
+                "A3\Characters_F\Heads\Data\hl_asian_bald_muscular_injury.rvmat"
+            };
+        };
+    };
+    class 16AA_SIGS_G4RS_soldier:SoldierWB {
+        scope = 1;
+        picture = "\2PARA_Equipment\Ui\2PARA_CA.paa";
+        model = "\2PARA_Equipment\Uniforms\Models\G4RS_16AA_SIGS.p3d";
+        uniformClass = "16AA_SIGS_G4RS_soldier";
         class Wounds {
             tex[] = {};
             mat[] = {
@@ -99,26 +163,6 @@ class CfgWeapons {
         class ItemInfo;
         class HeadgearItem;
     };
-    class U_SCOTT_ARMY_G4_uniform: Uniform_Base {
-        scope = 0;
-        displayName = "[2PARA] Crye G4 Uniform";
-        picture = "\2PARA_Equipment\Ui\2PARA_CA.paa";
-        model = "\2PARA_Equipment\Uniforms\Models\G4.p3d";
-        hiddenSelections[] = {
-            ""
-        };
-        hiddenSelectionsTextures[] = {
-            ""
-        };
-        class ItemInfo: UniformItem {
-            uniformModel = "-";
-            uniformClass = "SCOTT_ARMY_G4_soldier";
-            containerClass = "Supply40";
-            mass = 15;
-            armor = 1;
-            uniformType = "Neopren";
-        };
-    };
     class U_SCOTT_ARMY_G4RS_uniform: Uniform_Base {
         scope = 2;
         displayName = "[2PARA] Crye G4 Uniform(RS)";
@@ -140,27 +184,63 @@ class CfgWeapons {
         };
     };
      class U_SCOTT_ARMY_G4RS_RAMC_uniform: U_SCOTT_ARMY_G4RS_uniform {
-        scope = 0;
-        displayName = "[2PARA/RAMC] Crye G4 Uniform(RS)";
+        scope = 2;
+        displayName = "[16AA/RAMC] Crye G4 Uniform(RS)";
         picture = "\2PARA_Equipment\Ui\2PARA_CA.paa";
-        model = "\2PARA_Equipment\Uniforms\Models\G4RS.p3d";
+        model = "\2PARA_Equipment\Uniforms\Models\G4RS_16AA_RAMC.p3d";
         hiddenSelections[] = {
             "_TRFLH","_TRFRH","_AB"
         };
         hiddenSelectionsTextures[] = {
             "","\2PARA_Equipment\Helmets\Data\RAMC_CO.paa",""
         };
+         class ItemInfo: UniformItem {
+            uniformModel = "-";
+            uniformClass = "16AA_RAMC_G4RS_soldier";
+            containerClass = "Supply80";
+            mass = 15;
+            armor = 1;
+            uniformType = "Neopren";
+        };
      };
-      class U_SCOTT_ARMY_G4RS_SIGS_uniform: U_SCOTT_ARMY_G4RS_uniform {
-        scope = 0;
-        displayName = "[2PARA/SIGS] Crye G4 Uniform(RS)";
+      class U_SCOTT_ARMY_G4RS_RLC_uniform: U_SCOTT_ARMY_G4RS_uniform {
+        scope = 2;
+        displayName = "[16AA/RLC] Crye G4 Uniform(RS)";
         picture = "\2PARA_Equipment\Ui\2PARA_CA.paa";
-        model = "\2PARA_Equipment\Uniforms\Models\G4RS.p3d";
+        model = "\2PARA_Equipment\Uniforms\Models\G4RS_16AA_RLC.p3d";
         hiddenSelections[] = {
             "_TRFLH","_TRFRH","_AB"
         };
         hiddenSelectionsTextures[] = {
             "","\2PARA_Equipment\Helmets\Data\SIGS_CO.paa",""
+        };
+         class ItemInfo: UniformItem {
+            uniformModel = "-";
+            uniformClass = "16AA_RLC_G4RS_soldier";
+            containerClass = "Supply80";
+            mass = 15;
+            armor = 1;
+            uniformType = "Neopren";
+        };
+     };
+      class U_SCOTT_ARMY_G4RS_SIGS_uniform: U_SCOTT_ARMY_G4RS_uniform {
+        scope = 2;
+        displayName = "[16AA/SIGS] Crye G4 Uniform(RS)";
+        picture = "\2PARA_Equipment\Ui\2PARA_CA.paa";
+        model = "\2PARA_Equipment\Uniforms\Models\G4RS_16AA_SIGS.p3d";
+        hiddenSelections[] = {
+            "_TRFLH","_TRFRH","_AB"
+        };
+        hiddenSelectionsTextures[] = {
+            "","\2PARA_Equipment\Helmets\Data\SIGS_CO.paa",""
+        };
+         class ItemInfo: UniformItem {
+            uniformModel = "-";
+            uniformClass = "16AA_SIGS_G4RS_soldier";
+            containerClass = "Supply80";
+            mass = 15;
+            armor = 1;
+            uniformType = "Neopren";
         };
      };
 };
